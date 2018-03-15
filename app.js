71f41f6200777d2b237eb24beade3851171946de
Alexandr Voroncov <defauthack@gmail.com>
2018-03-15 09:12:44 +0300

    Add simple nodejs server

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('Test...');
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
